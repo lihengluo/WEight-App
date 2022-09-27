10e40fc819effab279697a59646622ec7ce6f403
peiqiangyu <peiqiangyu@hust.edu.cn>
2022-09-27 22:41:58 +0800

    Update layout

---
+++ app/src/main/java/fragment/bean/MainBean.java
@@ -0,0 +1,73 @@
+package fragment.bean;
+
+/**
+ * @Author : kezhijie
+ * @Email : 827112947@qq.com
+ * @Date : on 2022-09-27 15:06.
+ * @Description :描述
+ */
+public class MainBean {
+    private String img;
+    private String t1;
+    private String t2;
+    private String t3;
+    private String t4;
+    private String t5;
+
+    public MainBean(String img, String t1, String t2, String t3, String t4, String t5) {
+        this.img = img;
+        this.t1 = t1;
+        this.t2 = t2;
+        this.t3 = t3;
+        this.t4 = t4;
+        this.t5 = t5;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getT1() {
+        return t1;
+    }
+
+    public void setT1(String t1) {
+        this.t1 = t1;
+    }
+
+    public String getT2() {
+        return t2;
+    }
+
+    public void setT2(String t2) {
+        this.t2 = t2;
+    }
+
+    public String getT3() {
+        return t3;
+    }
+
+    public void setT3(String t3) {
+        this.t3 = t3;
+    }
+
+    public String getT4() {
+        return t4;
+    }
+
+    public void setT4(String t4) {
+        this.t4 = t4;
+    }
+
+    public String getT5() {
+        return t5;
+    }
+
+    public void setT5(String t5) {
+        this.t5 = t5;
+    }
+}
