93af778b8daf8de3b3d47d600f2257525d075ab4
yongjieYuan <11647427+yongjieyuan@user.noreply.gitee.com>
2022-09-27 10:11:26 +0800

    修改分析界面UI与逻辑

--- app/src/main/java/com/example/myapplication/activity/Analyze.java
+++ app/src/main/java/com/example/myapplication/activity/Analyze.java
@@ -1,9 +1,13 @@
 package com.example.myapplication.activity;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -16,11 +20,13 @@ import com.example.myapplication.database.CloudDB;
 import com.example.myapplication.function.CloudFunction;
 import com.example.myapplication.storage.CloudStorage;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
+import java.io.PrintStream;
 
 public class Analyze extends BaseActivity {
 
-    private ImageView cameraPicture;
     PhoneAuth phoneAuth = new PhoneAuth();
 
     @Override
@@ -28,25 +34,50 @@ public class Analyze extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.analyze);
 
-        cameraPicture = findViewById(R.id.same_picture);
-        cameraPicture.setImageBitmap(Camera.bitmap);
+        ImageView cameraPicture = findViewById(R.id.food_picture);
+
+        TextView foodNameText = findViewById(R.id.foodname);
+
+        TextView heatText = findViewById(R.id.heat);
+        TextView fatText = findViewById(R.id.text_fat);
+        TextView proteinText = findViewById(R.id.protein);
+        TextView carbohydrateText = findViewById(R.id.carbohydrate);
+        TextView caText = findViewById(R.id.text_ca);
+        TextView feText = findViewById(R.id.text_fe);
+
+        Button uploadDataBtn = findViewById(R.id.upload_to_cloud);
+
 
         final Intent myIntend = getIntent();
+
         String foodname = myIntend.getStringExtra("foodname");
+        foodNameText.setText(foodname);
         float heats = myIntend.getFloatExtra("heats",0);
+        heatText.setText(String.format("%s大卡", heats));
         float fat = myIntend.getFloatExtra("fat", 0);
+        fatText.setText(String.format("%s克", fat));
         float protein = myIntend.getFloatExtra("protein", 0);
+        proteinText.setText(String.format("%s克", protein));
         float Carbohydrates = myIntend.getFloatExtra("Carbohydrates", 0);
+        carbohydrateText.setText(String.format("%s克", Carbohydrates));
         float Ca = myIntend.getFloatExtra("Ca", 0);
+        caText.setText(String.format("%s克", Ca));
         float Fe = myIntend.getFloatExtra("Fe", 0);
+        feText.setText(String.format("%s克", Fe));
+
         String imgpath = myIntend.getStringExtra("imgpath");
-        if (!phoneAuth.isUserSignIn()) {
-            Toast.makeText(getApplicationContext(), "用户未登录！！！", Toast.LENGTH_SHORT).show();
-        } else {
-            if (!uploadToCloud(new File(imgpath), new Goods(null, foodname, heats, fat, protein, Carbohydrates, Ca, Fe))) {
-                Toast.makeText(getApplicationContext(), "上传失败，请重试！", Toast.LENGTH_SHORT).show();
+        cameraPicture.setImageBitmap(BitmapFactory.decodeFile(imgpath));
+
+
+        uploadDataBtn.setOnClickListener(view -> {
+            if (!phoneAuth.isUserSignIn()) {
+                Toast.makeText(getApplicationContext(), "您还未登录！！！", Toast.LENGTH_SHORT).show();
+            } else {
+                if (!uploadToCloud(new File(imgpath), new Goods(null, foodname, heats, fat, protein, Carbohydrates, Ca, Fe))) {
+                    Toast.makeText(getApplicationContext(), "上传失败，请重试！", Toast.LENGTH_SHORT).show();
+                }
             }
-        }
+        });
     }
 
     private boolean uploadToCloud(File imageFile, Goods good) {
