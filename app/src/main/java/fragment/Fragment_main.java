18087c0484190b86d51bc1eb60c5967a3f1b45dd
kaisenn <11377939+kaisenn@user.noreply.gitee.com>
2022-09-28 23:37:33 +0800

    更新建议

--- app/src/main/java/fragment/Fragment_main.java
+++ app/src/main/java/fragment/Fragment_main.java
@@ -109,12 +109,18 @@ public class Fragment_main extends Fragment {
     }
     public void changeImage() {
         int[] mArray = {
-                R.drawable.suggestion1,
-                R.drawable.suggestion2,
-                R.drawable.suggestion3,
-                R.drawable.suggestion4,
-                R.drawable.suggestion5,
-                R.drawable.suggestion6,
+//                R.drawable.suggestion1,
+//                R.drawable.suggestion2,
+//                R.drawable.suggestion3,
+//                R.drawable.suggestion4,
+//                R.drawable.suggestion5,
+//                R.drawable.suggestion6,
+                  R.drawable.suggestion7,
+                  R.drawable.suggestion8,
+                  R.drawable.suggestion9,
+                  R.drawable.suggestion10,
+                  R.drawable.suggestion11,
+                  R.drawable.suggestion12,
         };
         LinearLayout[] lArray = {
                 (LinearLayout) getView().findViewById(R.id.layout1),
