85e489e7d086d8dffad30e04e973e8a9bb22398b
kaisenn <11377939+kaisenn@user.noreply.gitee.com>
2022-10-09 11:35:54 +0800

    更新上传IP

--- app/src/main/java/com/example/myapplication/upload/UploadEngine.java
+++ app/src/main/java/com/example/myapplication/upload/UploadEngine.java
@@ -168,7 +168,9 @@ public class UploadEngine extends AUpDownloadEngine {
                 httpParams.put("param2", "value2");
 
                 // replace the url for upload
-                final String normalUrl = "http://192.168.1.29:5000/upload";
+                // final String normalUrl = "http://192.168.1.29:5000/upload";
+                // final String normalUrl = "http://124.71.153.95:5000/upload";
+                final String normalUrl = "http://139.9.91.253:5000/upload";
                 // upload file for http post
                 // replace the file path
                 JSONObject jsonObject = new JSONObject();
