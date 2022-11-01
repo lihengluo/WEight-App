6e9db30e8e926be61387ff62be7cf8a11fcdee4b
yongjieYuan <11647427+yongjieyuan@user.noreply.gitee.com>
2022-11-01 10:29:13 +0800

    迁移到新项目的配置

--- app/src/main/java/com/example/myapplication/activity/Main.java
+++ app/src/main/java/com/example/myapplication/activity/Main.java
@@ -175,20 +175,22 @@ public class Main extends BaseActivity {
             }
         });
 
-        try {
-            AGConnectOptionsBuilder builder = new AGConnectOptionsBuilder();
-            InputStream in = getAssets().open("agconnect-services.json");    //如果使用了AGC插件，删除此行
-            builder.setInputStream(in);
-            builder.setClientId("981817313709805184");
-            builder.setClientSecret("216C89C06BD713DC0F94A440D6ACDB4052B12B4468331D675F412EFFD60270DC");
-            builder.setApiKey("DAEDAOdUgkjCnRD4/xfDwBnv3MOJzw6aUT0CQ0VxUgfSPe99ZDD0lmvHfffuffK4uHp4bTYpQeXRdWDm2EXsH7I2G/O/0EuFy5Tzcw==");
-            builder.setCPId("420086000304642213");
-            builder.setProductId("99536292102615511");
-            builder.setAppId("107062115");
-            AGConnectInstance.initialize(this, builder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        AGConnectOptionsBuilder builder = new AGConnectOptionsBuilder();
+        AGConnectInstance.initialize(this, builder);
+//        try {
+//            AGConnectOptionsBuilder builder = new AGConnectOptionsBuilder();
+//            InputStream in = getAssets().open("agconnect-services.json");    //如果使用了AGC插件，删除此行
+//            builder.setInputStream(in);
+//            builder.setClientId("981817313709805184");
+//            builder.setClientSecret("216C89C06BD713DC0F94A440D6ACDB4052B12B4468331D675F412EFFD60270DC");
+//            builder.setApiKey("DAEDAOdUgkjCnRD4/xfDwBnv3MOJzw6aUT0CQ0VxUgfSPe99ZDD0lmvHfffuffK4uHp4bTYpQeXRdWDm2EXsH7I2G/O/0EuFy5Tzcw==");
+//            builder.setCPId("420086000304642213");
+//            builder.setProductId("99536292102615511");
+//            builder.setAppId("107062115");
+//            AGConnectInstance.initialize(this, builder);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         // 发现已经有用户登录时立即跳转
         if (phoneAuth.isUserSignIn()) {
